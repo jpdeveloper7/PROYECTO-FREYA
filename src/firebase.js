19193ed4a54ab79a90f30097e144fd7f314e1793
jotaplay7 <jp.arenas94@gmail.com>
2026-05-23 17:53:21 -0500

    chore: unify firebase config and re-export duplicate file; move config to env vars

--- src/firebase.js
+++ src/firebase.js
@@ -17,13 +17,13 @@ export const resetPassword = async (email) => {
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPm8fxe7qDVn4-UTWKDKfDBOzOTPAbg7A",
-  authDomain: "freya-app-bd.firebaseapp.com",
-  projectId: "freya-app-bd",
-  storageBucket: "freya-app-bd.firebasestorage.app",
-  messagingSenderId: "302665316692",
-  appId: "1:302665316692:web:5c2c57b0827c4e843530c1",
-  measurementId: "G-B09LYXNH45"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyAPm8fxe7qDVn4-UTWKDKfDBOzOTPAbg7A",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "freya-app-bd.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "freya-app-bd",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "freya-app-bd.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "302665316692",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:302665316692:web:5c2c57b0827c4e843530c1",
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-B09LYXNH45",
 };
 
 const app = initializeApp(firebaseConfig);
